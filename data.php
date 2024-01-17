a00e599560a1446bd3ef874a65507ca9711fe9e9
Dara Kania Damanik <110018761+darakd1707@users.noreply.github.com>
2024-01-17 14:26:34 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'cafe_ta');
+$conn = mysqli_connect('localhost', 'admnin', 'daradk17', 'cafe_ta');
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
