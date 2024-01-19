87221582da1994715e219f5ebaf3b253df63ba96
Dara Kania Damanik <110018761+darakd1707@users.noreply.github.com>
2024-01-19 17:59:07 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('cafeta.cnaea042gafi.us-west-1.rds.amazonaws.com', 'admnin', 'daradk17', 'cafeta');
+$conn = mysqli_connect('cafeta.cnaea042gafi.us-west-1.rds.amazonaws.com', 'admin', 'daradk17', 'cafeta');
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
